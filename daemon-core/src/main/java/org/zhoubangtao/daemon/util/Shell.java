916c8ea35259fce83abe8dccfb0dbcb6b00800b0
zhoubangtao <zhoubangtao@gmail.com>
2014-03-19 04:24:24 +0800

    modify Shell and add ShutdownHookManager,ShutdownThreadHelper

--- daemon-core/src/main/java/org/zhoubangtao/daemon/util/Shell.java
+++ daemon-core/src/main/java/org/zhoubangtao/daemon/util/Shell.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.sql.Time;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Timer;
